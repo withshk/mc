36efff4f2244eb883c7941ee2dadff1dbd16ffa3
withshk <withshk0527@gmail.com>
2026-07-02 10:10:25 +0900

    .

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ mouse_sensitivity = Vec2(40,40) # Default: (40,40)
 run_speed = 5 # Default: 5
 
 window.fps_counter.enabled = False
-window.exit_button.visible = False
+window.exit_button.visible = False 
  
 punch = Audio('assets/punch', autoplay=False)
    
